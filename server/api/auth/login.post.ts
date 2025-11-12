d4aea3b53d37072220f53e584806248cda24afc9
Niño Raleigh Y. Paragas <ninoraleighparagas@gmail.com>
2025-11-12 16:32:08 +0800

    .

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -34,7 +34,7 @@ export default defineEventHandler(async (event) => {
 
   const token = jwt.sign({id: foundUser.id , username: foundUser.username}, process.env.JWT_PRIVATE!, {
     algorithm: 'HS256',
-    expiresIn: '10s'
+    expiresIn: '7d'
   })
 
   // ✅ Return successful login
